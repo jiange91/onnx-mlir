dad310bd1de14347dd6d8c5f22e58057b9a1574e
jiange91 <zih015@ucsd.edu>
2023-02-20 02:55:02 -0700

    feat: outside onnx runtime

--- include/common.h
+++ include/common.h
@@ -7,3 +7,27 @@ static inline uint64_t getCurNs() {
   uint64_t t = ts.tv_sec*1000*1000*1000 + ts.tv_nsec;
   return t;
 }
+
+#define DEF_TENSOR(r) \
+typedef struct { \
+  float *_alloc_ptr; \
+  float *_aligned_ptr; \
+  int64_t offset; \
+  int64_t shapes[r]; \
+  int64_t strides[r]; \
+} Tensor_R##r
+
+#define MAKE_TENSOR(t, data, shape, r) \
+  Tensor_R##r _T_##t; \
+  do { \
+    _T_##t._alloc_ptr = data; \
+    _T_##t._aligned_ptr = data; \
+    _T_##t.offset = 0; \
+    for (int i = r-1; i >= 0; -- i) { \
+      _T_##t.shapes[i] = shape[i]; \
+      if (i == (r-1)) \
+        _T_##t.strides[i] = 1; \
+      else \
+        _T_##t.strides[i] = shape[i+1] * _T_##t.strides[i+1];  \
+    } \
+  } while (0)
---
+++ pl-app/raw_main.c
@@ -0,0 +1,56 @@
+#include "common.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define abs(X) ((X) < 0 ? -1.f * (X) : (X))
+DEF_TENSOR(2);
+DEF_TENSOR(4);
+
+void _mlir_ciface_main_graph(Tensor_R2 *y, Tensor_R4 *input);
+
+float *read_tensor(const char* file, int64_t shape[], int rank) {
+  size_t num_ele = 1;
+  for (int i = 0; i < rank; ++ i)
+    num_ele *= shape[i];
+
+  float *dat_buf = (float *) malloc(sizeof(float) * num_ele);
+
+  FILE *datfile = fopen(file, "r");
+  size_t read = fread(dat_buf, sizeof(float), num_ele, datfile);
+  if (read < num_ele)
+    printf("Error reading input np array from %s\n", file);
+
+  fclose(datfile);
+  return dat_buf;
+}
+
+int main(int argc, char *argv[]) {
+  // Build the inputs, starts with shared shape & rank.
+  int64_t in_shape[] = {10000, 1, 28, 28};
+  int64_t out_shape[] = {10000, 10};
+  int64_t rank = 4;
+
+  float *in_buf = read_tensor("dummy_64_fminist_in.dat", in_shape, rank);
+  float *out_buf = read_tensor("dummy_64_fminist_out.dat", out_shape, 2);
+
+  MAKE_TENSOR(x, in_buf, in_shape, 4);
+  Tensor_R2 output;
+
+  // Compute outputs.
+  uint64_t start_ns = getCurNs();
+  _mlir_ciface_main_graph(&output, &_T_x);
+  uint64_t end_ns = getCurNs();
+  printf("Exec time %.6f us\n", (float)(end_ns - start_ns)/1e3);
+
+  // Get the first omt as output.
+  printf("Verify output ---- \n");
+  float *outputPtr = output._aligned_ptr;
+  for (int64_t e = 0; e < out_shape[0] * out_shape[1]; ++ e) {
+    float diff = abs(outputPtr[e] - out_buf[e]);
+    if (diff >= 1e-5)
+      printf("pred: %f, y: %f, diff: %f\n", outputPtr[e], out_buf[e], diff);
+  }
+  printf(" ---- Complete\n");
+  return 0;
+}
